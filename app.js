c12551a356377af9b6e0e5a8212c1674b6fbbb79
shafu.eth <selfouly@gmail.com>
2022-12-16 20:24:35 +0100

    works

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const dNFT = "0x2544bA4Bc4A1d4Eb834a2770Fd5B52bAfa500B44";
-const N_NFTS = 300;
+const POOL = "0xC98D30Cf8837dE6ae019D37084f1893751D47C4E";
 const INFURA = `wss://goerli.infura.io/ws/v3/${process.env.GOERLI_INFURA_PROJECT_ID}`;
-console.log(INFURA);
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
@@ -17,6 +16,7 @@ const supabase = createClient(
   process.env.SUPABASE_KEY
 );
 
+var web3 = new Web3(INFURA);
 Contract.setProvider(INFURA);
 
 function sortByXp(nfts) {
@@ -25,7 +25,11 @@ function sortByXp(nfts) {
   });
 }
 
-async function setup() {
+async function updateNftTable(nfts) {
+  const { error } = await supabase.from("nfts").upsert(sortByXp(nfts));
+}
+
+async function refresh() {
   var contract = new Contract(dNFT_ABI["abi"], dNFT);
   const totalSupply = await contract.methods.totalSupply().call();
 
@@ -35,85 +39,30 @@ async function setup() {
     console.log(i);
     const tokenId = await contract.methods.tokenByIndex(i).call();
     const nft = await contract.methods.idToNft(tokenId).call();
-    nfts.push(nft);
-    console.log(nft);
-    if (i == 5) {
-      break;
-    }
+    nfts.push({
+      id: tokenId,
+      xp: nft.xp,
+    });
   }
 
-  console.log(sortByXp(nfts));
+  await updateNftTable(sortByXp(nfts));
+  return 0;
 }
 
-setup();
-
-async function setUpNftTable() {
-  // If row exists it will not be overwritten!
-  var rows = [];
-  for (let i = 0; i < N_NFTS; i++) {
-    rows.push({ index: i });
-  }
-  const { error } = await supabase.from("nfts").upsert(rows);
-  console.log(error);
-}
-
-async function syncXP() {
-  var contract = new Contract(dNFT_ABI, dNFT);
-  var rows = [];
-  for (let i = 0; i < N_NFTS; i++) {
-    console.log(i);
-    const xp = await contract.methods.xp(i).call();
-    rows.push({ index: i, xp: xp });
-  }
-  const { error } = await supabase.from("nfts").upsert(rows);
-  console.log(error);
-}
-
-async function subXP() {
-  /*
-   * we need to check if the old value was not max or min!
-   */
-
-  // just for testing
-  const newXP = 11500;
-
-  // always has column id 0
-  const { data } = await supabase.from("sum").select().eq("id", 0);
-  console.log(data);
-  const minXP = parseInt(data[0].minXP);
-  const maxXP = parseInt(data[0].maxXP);
-
-  if (newXP < minXP) {
-    console.log("newXP < minXP");
-    const { error } = await supabase
-      .from("sum")
-      .update({ minXP: newXP })
-      .eq("id", 0);
-    console.log(error);
-  }
-
-  if (newXP > maxXP) {
-    console.log("newXP > minXP");
-    const { error } = await supabase
-      .from("sum")
-      .update({ maxXP: newXP })
-      .eq("id", 0);
-  }
-}
-
-function sub() {
-  var web3 = new Web3(INFURA);
+async function subscribeToSync() {
+  await refresh();
+  console.log("subscribing to sync");
   var subscription = web3.eth
     .subscribe(
       "logs",
       {
-        address: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", // weth
+        address: POOL,
         topics: [
-          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+          "0xff14d8520387b9b85d2a017dc41ae15db04a22d4c67deac04eb45048631ffa86",
         ],
       },
       function (error, result) {
-        console.log(3333);
+        await refresh();
         if (!error) console.log(result);
       }
     )
@@ -132,27 +81,4 @@ function sub() {
     });
 }
 
-async function calcAverageXP() {
-  var average = 0;
-  const { data } = await supabase.from("nfts").select("xp");
-  data.map((d) => {
-    average += parseInt(d.xp);
-  });
-  average = average / N_NFTS;
-  console.log(average);
-  const { error } = await supabase
-    .from("sum")
-    .update({ averageXP: average })
-    .eq("id", 0);
-  console.log(error);
-  // console.log(data);
-}
-
-// syncXP();
-// subXP();
-// const ss = sub();
-// calcAverageXP();
-
-// sub();
-// console.log(web3);
-// setUpNftTable();
+subscribeToSync();
